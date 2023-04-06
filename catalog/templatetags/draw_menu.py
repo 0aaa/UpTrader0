be7ac444567772f8906b2123297bb2e9819f18a2
0aaa <barneycalhoun256@gmail.com>
2023-04-06 06:02:49 +0300

    Getting of parent menus in the full form added v0.

--- catalog/templatetags/draw_menu.py
+++ catalog/templatetags/draw_menu.py
@@ -1,26 +1,29 @@
 from django.shortcuts import get_object_or_404
-from django.urls import reverse
-from ..models import Menu
 from django import template
+from ..models import Menu
 
 register = template.Library()
 @register.inclusion_tag('catalog/menu.html')
-def draw_menu(menu_name='test1'):
+def draw_menu(menu_name):
+    if menu_name is None:
+        return None
     current_menu = get_object_or_404(Menu, name=menu_name)
-    parents = []
+    content = {
+        'current_menu': menu_name,
+        'parents': []
+    }
     childs = Menu.objects.filter(parent=current_menu.id)
     if childs.count() > 0:
-        grandChilds = Menu.objects.filter(parent=childs[0].id)
-    else:
-        grandChilds = []
+        grand_childs = Menu.objects.filter(parent=childs[0].id)
+        if grand_childs is not None:
+            content['grand_childs'] = grand_childs
+        content['childs'] = childs
 
-    parent_menu = current_menu
-    while parent_menu.parent is not None:
-        parent_menu = get_object_or_404(Menu, pk=parent_menu.parent.id)
-        parents.insert(0, parent_menu)
+    while current_menu.parent is not None:
+        parent_menu = Menu.objects.filter(parent=current_menu.parent)
+        current_menu = get_object_or_404(Menu, pk=current_menu.parent.id)
+        content['parents'].append(parent_menu)
+    content['parents'].reverse()
     return {
-        'parents': parents,
-        'current_menu': current_menu,
-        'childs': childs,
-        'grandChilds': grandChilds
+        'content': content
     }
